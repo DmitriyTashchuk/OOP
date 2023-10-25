9df7d40da40dfa3790b1d9f9015ef8003f4274a7
Dmitriy <tashukdima4433@gmail.com>
2023-10-25 17:24:43 +0300

    ///

--- Lab11/Source.cpp
+++ Lab11/Source.cpp
@@ -4,20 +4,23 @@
 #include <iomanip>
 #include <vector>
 using namespace std;
-struct Grade {
+struct Grade { //структура з оцінками
 	int math;
 	int chemistry;
 	int physics;
 	int history;
+	int oop;
+	int prog;
+	int web;
 };
-struct Student {
+struct Student { // структура студентів
 	char name[10];
 	int course;
 	char group[10];
 	Grade grade;
 
 };
-int count_grade(Student student) {
+int count_grade(Student student) { //підрахування кількості предметів на відмінно
 	int count = 0;
 	if (student.grade.chemistry == 5) {
 		count++;
@@ -32,28 +35,50 @@ int count_grade(Student student) {
 		count++;
 	}
 	return count;
-}
-void RW(string student) {
+} 
+void RW(string student) { //функція запису в текстовий файл
 	ofstream file_write("file.txt", ios::app);
 	file_write << student << endl;
 	file_write.close();
 }
-void RW(Student student) {
+void RW(Student student) { // функція запису в типізований файл
 	ofstream file_write("file.dat", ios::app|ios::binary);
 	file_write.write((char*)&student, sizeof(Student));
 	file_write.close();
 }
-void RD(string filename) {
+void RD_for_olimpiad(string filename, string filename2) { // функція відбору студентів на олімпіаду
+	vector<Student> students;
+	//зчитування даних з файлу
+	ifstream file_read(filename);
+	//відкриття типізованого для запису вибраної інформації
+	ofstream file_write(filename2, ios::trunc | ios::binary);
+	cout << "Cтуденти, відібрані для олімпіади:" << endl;
+	while (file_read) {
+		Student student;
+		file_read >> student.name >> student.course >> student.group >> student.grade.math >> student.grade.chemistry >> student.grade.physics >> student.grade.history >> student.grade.oop >> student.grade.prog >> student.grade.web;
+		if (!file_read.eof() && student.grade.oop == 5 && student.grade.prog == 5 && student.grade.web == 5) {
+			file_write.write((char*)&student, sizeof(Student));
+			cout << "Ім'я: " << student.name << " Курс: " << student.course << endl;
+			cout << "Оцінки: ООП: " << student.grade.oop << " Програмування: " << student.grade.prog << " Веб-технології: " << student.grade.web << endl;
+		}
+	}
+	file_write.close();
+	file_read.close();
+}
+void RD(string filename) { // функція зчитування з текстового
 	vector<Student> students;
 	ifstream file_read(filename);
-	while (file_read){
+	while (file_read){ 
+		// вивід з текстового файлу
 		Student student;
-		file_read >> student.name >> student.course >> student.group >> student.grade.math >> student.grade.chemistry >> student.grade.physics >> student.grade.history;
+		file_read >> student.name >> student.course >> student.group >> student.grade.math >> student.grade.chemistry >> student.grade.physics >> student.grade.history >> student.grade.oop >> student.grade.prog >> student.grade.web;
+		/*file_read >> student.name >> student.course >> student.group >> student.grade.math >> student.grade.chemistry >> student.grade.physics >> student.grade.history;*/
 		if (!file_read.eof()) {
 			students.push_back(student);
 		}
 	}
 	file_read.close();
+	// сортування по зданих предметах на відмінно
 	int length = students.size();
 	vector<string> used_group;
 	for (int i = 0; i < length; i++) {
@@ -72,6 +97,7 @@ void RD(string filename) {
 					}
 				}
 			}
+			//вивід впорядкованої інформації
 			cout << "Група: " << current_group << endl;
 			for (int j = 0; j < temp.size(); j++) {
 				cout << "Ім'я: " << temp[j].name << " Курс: " << temp[j].course << endl;
@@ -81,13 +107,13 @@ void RD(string filename) {
 		}
 	}
 }
-void RD(string filename, int trash) {
+void RD(string filename, int trash) { //функція зчитування з типізованого файлу
 	ifstream file_read(filename, ios::binary);
 	float hist = 0, math = 0, phys = 0, chem = 0, count = 0;
 	while (file_read) {
 		Student student;
 		file_read.read((char*)&student, sizeof(Student));
-		if (!file_read.eof()) {
+		if (!file_read.eof()) { //підрахування суми оцінок з предметів по всіх студентах
 			hist += student.grade.history;
 			math += student.grade.math;
 			phys += student.grade.physics;
@@ -95,13 +121,15 @@ void RD(string filename, int trash) {
 			count++;
 		}
 	}
+	//вивід середніх значень по предметах
 	cout << "Середні бали по предметах : " << endl;
 	cout << "Математика: " << setprecision(2) << math / count << " Хімія: " << chem / count << endl;
 	cout << "Фізика: " << phys / count << " Історія: " << hist / count << endl;
 	file_read.close();
 }
-int main() {
+int main() { // основна функція
 	setlocale(LC_ALL, "ukrainian");
+	//ввід даних студентів і запис в файли
 	/*int number;
 	cout << "Введіть кількість студентів" << endl;
 	cin >> number;
@@ -117,7 +145,9 @@ int main() {
 		RW(student);
 		RW(student_string);
 	}*/
+	//функції зчитування
 	RD("file.txt");
 	RD("file.dat", 1);
+	RD_for_olimpiad("file.txt", "file2.dat");
 	cin.get();
 }
